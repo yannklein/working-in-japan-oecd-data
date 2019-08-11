c9fa4c07099c46d9eac9fc8a470e9149337eabb9
Yann Klein <yann.klein@me.com>
2019-08-11 12:02:35 +0900

    new data update

---
+++ update.sh
@@ -0,0 +1,5 @@
+jupyter nbconvert --to notebook --execute working_in_japan.ipynb
+jupyter nbconvert working_in_japan.ipynb --to html --template full --output working_in_japan.html
+git add .
+git commit -m "new data update"
+git push origin master
