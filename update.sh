e8cab51238ad5bbf2f35d5d8dd5718aa4ffc9393
Yann Klein <yann.klein@me.com>
2019-08-11 13:46:30 +0900

    new data update

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 export PATH=~/anaconda3/bin:$PATH
 jupyter nbconvert --to notebook --execute working_in_japan.ipynb
-jupyter nbconvert working_in_japan.ipynb --to html --template full --output working_in_japan.html
+jupyter nbconvert working_in_japan.ipynb --to html --template hide_code --output working_in_japan.html
 git add .
 git commit -m "new data update"
 git push origin master
